77800caaff9b16deff9df78d1715d73c641f265d
FAHIM TAYEBEE <fahimtayebee9@gmail.com>
2020-08-04 09:23:55 +0600

    Profile Php Updated

--- Mid_Lab_Exam/layouts/profile.php
+++ Mid_Lab_Exam/layouts/profile.php
@@ -17,27 +17,37 @@
         $file = fopen('../php/user.txt', 'r');
         $data = fread($file, filesize('../php/user.txt'));
         $user = explode('|', $data);
+        if($_SESSION['id'] == $user[0]){
+            $valid_user = [$user[0],$user[1],$user[2],$user[3],$user[4]];
     ?>
-    <table>
-        <tr>
-            <td colspan=2 style="text-align: center;">Profile</td>
-        </tr>
-        <tr>
-            <td>Id </td>
-            <td><?php echo $user[0]?></td>
-        </tr>
-        <tr>
-            <td>Name </td>
-            <td><?php echo $user[2]?></td>
-        </tr>
-        <tr>
-            <td>Email </td>
-            <td><?php echo $user[3]?></td>
-        </tr>
-        <tr>
-            <td>User Type </td>
-            <td><?php echo $user[4]?></td>
-        </tr>
-    </table>
+        <table>
+            <tr>
+                <td colspan=2 style="text-align: center;">Profile</td>
+            </tr>
+            <tr>
+                <td>Id </td>
+                <td><?php echo $user[0]?></td>
+            </tr>
+            <tr>
+                <td>Name </td>
+                <td><?php echo $user[2]?></td>
+            </tr>
+            <tr>
+                <td>Email </td>
+                <td><?php echo $user[3]?></td>
+            </tr>
+            <tr>
+                <td>User Type </td>
+                <td><?php echo $user[4]?></td>
+            </tr>
+        </table>
+    <?php
+        }
+        else{
+            echo "Invalid User Data";
+        }
+
+    ?>
+    
 </body>
 </html>
