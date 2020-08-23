01a0624dac5cf4ad18aac3875a557a024e0d1acd
FAHIM TAYEBEE <fahimtayebee9@gmail.com>
2020-08-24 02:07:12 +0600

    Name Script JS

--- FINAL_LAB_TASK_2/Name/script.js
+++ FINAL_LAB_TASK_2/Name/script.js
@@ -1,11 +1,23 @@
 function validation(){
-
-}
-
-function remover(){
+	var name = document.getElementById('name').value;
+	var errorElement = document.getElementById('nameWarnigng');
+	if(name == "" || name == null){
+		errorElement.innerHTML = "Name can't left empty";
+		return false;
+	}
+	else if(name.split(" ").length < 2){
+		document.getElementById('nameWarnigng').innerHTML = "Name must be atleast 2 words";
+		return false;
+	}else{
+		return true;
+	}
 	
+function remover(){
+	document.getElementById('nameWarnigng').innerHTML = "";
 }
 
 function blurText(){
-	
+	if(document.getElementById('name').value == ""){
+		document.getElementById('nameWarnigng').innerHTML = "this field is required!";
+	}
 }
